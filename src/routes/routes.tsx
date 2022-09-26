7b05e6a0f6e05e017de9a06804449158ca5d403c
sshmm <s.shawky92@gmail.com>
2022-09-27 01:08:39 +0200

    Add nomination page and access rules

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -13,7 +13,11 @@ import {
 
 import AddMembersPage from '../pages/AddMembersPage/AddMembersPage';
 import JoinCirclePage from '../pages/JoinCirclePage';
-import { useFixCircleState, useRoleInCircle } from 'hooks/migration';
+import {
+  useCanVouch,
+  useFixCircleState,
+  useRoleInCircle,
+} from 'hooks/migration';
 import AdminCircleApiPage from 'pages/AdminCircleApiPage/AdminCircleApiPage';
 import AdminPage from 'pages/AdminPage';
 import AllocationPage from 'pages/AllocationPage';
@@ -26,6 +30,7 @@ import DevPortalPage from 'pages/DevPortalPage';
 import DistributionsPage from 'pages/DistributionsPage';
 import HistoryPage from 'pages/HistoryPage';
 import IntegrationCallbackPage from 'pages/IntegrationCallbackPage';
+import { NewNominationPage } from 'pages/NewNominationPage/NewNominationPage';
 import ProfilePage from 'pages/ProfilePage';
 import VaultsPage from 'pages/VaultsPage';
 import { VaultTransactions } from 'pages/VaultsPage/VaultTransactions';
@@ -52,6 +57,9 @@ export const AppRoutes = () => {
         <Route path="members" element={<AdminPage />} />
         <Route path="members/add" element={<AdminRouteHandler />}>
           <Route path="" element={<AddMembersPage />} />
+        </Route>
+        <Route path="members/nominate" element={<VouchingRouteHandler />}>
+          <Route path="" element={<NewNominationPage />} />
         </Route>
         <Route path="admin" element={<AdminRouteHandler />}>
           <Route path="" element={<CircleAdminPage />} />
@@ -118,5 +126,14 @@ const AdminRouteHandler = () => {
 
   if (!isUserAdmin({ role }))
     return <Redirect to={paths.home} note="not admin" />;
+  return <Outlet />;
+};
+
+const VouchingRouteHandler = () => {
+  const params = useParams();
+  const circleId = Number(params.circleId);
+  const canVouch = useCanVouch(circleId);
+
+  if (!canVouch) return <Redirect to={paths.home} note="not admin" />;
   return <Outlet />;
 };
